6894f608cc051d2c7d6fef5790a97a42f17fcde9
LukaRoguljic <luka.roguljic16@gmail.com>
2018-04-16 17:00:21 +0200

    Step 1: Added Activities and transitions between

--- app/src/main/java/com/example/luka/todotask/MainActivity.java
+++ app/src/main/java/com/example/luka/todotask/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.luka.todotask;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnAddCat)   Button btnAddCategory;
+    @BindView(R.id.btnAddTask)  Button btnAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnAddCat)
+    public void startAddCategory(){
+        Intent startAddCatActivity = new Intent(this,AddCategory.class);
+        startActivity(startAddCatActivity);
+    }
+
+    @OnClick(R.id.btnAddTask)
+    public void startAddTask(){
+        Intent startAddTaskActivity = new Intent(this,AddTask.class);
+        startActivity(startAddTaskActivity);
     }
 }
